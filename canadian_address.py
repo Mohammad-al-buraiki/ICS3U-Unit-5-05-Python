ffffc5708a982168583814cf6aff1fea0d0b0d17
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2021-01-07 17:01:12 +0000

    canadian_address.py

---
+++ canadian_address.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on January 2021
+# This program is canadian address program
+
+
+import constants
+
+def main():
+    name = input ("enter: ")
+     
+    if name = constants.province:
+         print("here we go{0}.".format(name))
+
+    else:
+        print("no no")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on January 2021
+# This program is canadian address program
+
+province = "ontario" or "ottawa"
